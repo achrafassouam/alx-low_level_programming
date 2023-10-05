29c3b180ebd88eada11edd29c4b293090d839783
Achraf Assouam <achrafassouam@gmail.com>
2023-10-05 15:43:36 -0700

    adding 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a specific text to standard output
+ *
+ * Return: 1 (Success)
+*/
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19);
+	return (1);
+}
