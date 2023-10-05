e9f1051d0848d23df9a698887143a25e7c0aac7f
Achraf Assouam <achrafassouam@gmail.com>
2023-10-05 12:31:13 -0700

    add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Start of program
+ *
+ * Description: To print a string using printf
+ *
+ * Return: 0 when program run correctly
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
