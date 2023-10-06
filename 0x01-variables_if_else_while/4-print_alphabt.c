0d43dc2d6f372058a9b89d2bf75e1a4f137b8739
Achraf Assouam <achrafassouam@gmail.com>
2023-10-06 14:51:50 -0700

    adding 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * except e and q
+ * Return: 0 when success
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while
+	(letter < 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+			letter++;
+	}
+	putchar('\n');
+	return (0);
+}
