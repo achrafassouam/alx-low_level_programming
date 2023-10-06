fdc516d5d0972e1e5e51c0d81272c3d7b70cd247
Achraf Assouam <achrafassouam@gmail.com>
2023-10-06 15:12:17 -0700

    5-print_numbers.c - adding file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 0 to 9
+ * Return: 0 when success
+ */
+int main(void)
+{
+	int n = 0;
+
+	while
+	(n <= 9)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
