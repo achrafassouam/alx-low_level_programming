8e07939c07a81bb6f78d60f5747e61f8bf06f016
Achraf Assouam <achrafassouam@gmail.com>
2023-10-10 11:35:35 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - ..
+ *
+ * Return: 0 on success
+ */
+
+void print_alphabet(void);
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
