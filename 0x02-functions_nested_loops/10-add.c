aeb850876c390b7f0f90f0a627112cbc28b9835e
Achraf Assouam <achrafassouam@gmail.com>
2023-10-10 17:17:25 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - script that adds 2 ints
+ * @a: type int
+ * @b: type int
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (c);
+}
