89ef40f2fd79ab64d0e56c6402610419bb007411
Achraf Assouam <achrafassouam@gmail.com>
2023-10-12 04:24:04 -0700

    adding 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * mul - function to return multiple of 2 int
+ * @a: int type input
+ * @b: int type input
+ * Return: mult when success
+ */
+
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}
