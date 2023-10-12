bcd2ab392ab34a593801bb8bb1bd61bf29f4f51f
Achraf Assouam <achrafassouam@gmail.com>
2023-10-12 04:59:41 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers except 2 and 4
+ * Return: 0 on success
+ */
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		if (n != 2 && n != 4)
+		{
+		_putchar(n + '0');
+		}
+		n++;
+	}
+}
