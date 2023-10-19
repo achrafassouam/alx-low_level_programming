9d8d30147cacfd4c1428936d70e3a3bcf5d450bb
Achraf Assouam <achrafassouam@gmail.com>
2023-10-19 16:25:31 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * rot13 - convert text to rot13
+ * @s: char type pointer
+ * Return: s
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char x;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		x = 0;
+		j = 0;
+
+		while (in[j\ != '\0' && x == 0)
+		{
+			if (s[i] == in[j])
+			{
+				s[i] = out[j];
+				x = 1;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+
+
+}
