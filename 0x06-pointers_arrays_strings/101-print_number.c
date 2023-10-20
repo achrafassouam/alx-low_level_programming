25abe634df603e67ec913475891e36ddf2212768
Achraf Assouam <achrafassouam@gmail.com>
2023-10-20 15:55:36 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - to print numbers stored in n
+ * @n: int type input
+ */
+void print_number(int n)
+{
+	int i = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+
+	i = n;
+
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + 48);
+}
