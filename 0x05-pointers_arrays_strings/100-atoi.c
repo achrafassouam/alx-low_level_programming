ce89246ee1a04335628828acf63b954fdf593b24
Achraf Assouam <achrafassouam@gmail.com>
2023-10-21 11:31:49 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: char type pointer
+ */
+
+int _atoi(char *s)
+{
+	int i, j, k, res;
+
+	i = res = j = 0;
+	k = -1;
+    for (k = -1; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			k = k * -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10;
+			res = res - (s[i] - '0');
+			j = 1;
+		}
+		else if (j == 1)
+			break;
+	}
+	res *= k;
+	return (res);
+}
