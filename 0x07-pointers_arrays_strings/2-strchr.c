b3bff176173ff63f4ea9838de419a63f53b2df9d
Achraf Assouam <achrafassouam@gmail.com>
2023-10-23 07:55:36 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strchr - locates a character in a string
+ * @s: char type pointer
+ * @c: char type input
+ * Return: s if true, NULL if otherwise
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return ((char *) s);
+		s++;
+	}
+	return (NULL);
+}
