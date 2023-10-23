4b2318fe36396058d7463c6f91a7267f8aa90672
Achraf Assouam <achrafassouam@gmail.com>
2023-10-23 12:50:22 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: char type pointer
+ * @needle: char type pinter
+ * Return: haystack if found match, NULL otherwise
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+		{
+			char *c = haystack;
+			char *d = needle;
+
+			while (*d != '\0' && *c == *d)
+			{
+				c++;
+				d++;
+			}
+			if (*d == '\0')
+			{
+				return ((char *) haystack);
+			}
+
+		}
+		haystack++;
+	}
+	return (NULL);
+}
