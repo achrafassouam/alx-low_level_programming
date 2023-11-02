66053abb274036b7821a99e804c8138f0f897054
Achraf Assouam <achrafassouam@gmail.com>
2023-11-02 07:03:38 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * string_nconcat - concatenates two strings
@@ -12,47 +13,27 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int len1, len2, sbuffer, i;
+	unsigned int len1, len2, i;
 
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
 
-	sbuffer = len1 + len2 + 1;
+	if (n < len2)
+		len2 = n;
 
-	p = malloc(sizeof(char) * sbuffer);
-
+	p = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (p == NULL)
 		return (NULL);
-
-	i = 0;
-
-	while (i < len1)
+	for (i = 0; i < (len1 + len2); i++)
 	{
-		p[i] = s1[i];
-		i++;
+		if (i < len1)
+			p[i] = s1[i];
+		else
+			p[i] = s2[i - len1];
 	}
+	p[i] = '\0';
 
-	if (n >= len2)
-	{
-		i = 0;
-
-		while (i < len2)
-		{
-			p[len1 + i] = s2[i];
-			i++;
-		}
-	}
-	else
-	{
-		i = 0;
-
-		while (i < n)
-		{
-			p[len1 + i] = s2[i];
-			i++;
-		}
-	}
 	return (p);
 }
