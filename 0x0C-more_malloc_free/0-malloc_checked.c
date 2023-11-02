f012dca70807cabaf547d6c5ab8b9ebb860817ff
Achraf Assouam <achrafassouam@gmail.com>
2023-11-02 06:12:29 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: unsigned int type input for buffer size
+ * Return: pointer to buffer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+
+	return (p);
+}
