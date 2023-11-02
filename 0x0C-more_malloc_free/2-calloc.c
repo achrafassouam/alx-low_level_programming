ad588ecadc98176f5eeb4bf28434fa9c2c6f5035
Achraf Assouam <achrafassouam@gmail.com>
2023-11-02 14:23:59 -0700

    2-calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p;
+	char *p;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i <= nmemb; i++)
+	for (i = 0; i < nmemb; i++)
 	{
 		p[i] = 0;
 	}
