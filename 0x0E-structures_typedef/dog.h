65ce8df837653742cf8508c9e26f73d0b75dde33
Achraf Assouam <achrafassouam@gmail.com>
2023-11-05 14:31:46 -0800

    dog_t typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,8 @@ struct dog
 
 /* Additional functions */
 
+typedef struct dog dog_t;
+
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
