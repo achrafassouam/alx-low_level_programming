c108972b9f0cb0e1726383367a0921b37054c91b
achraf assouam <achrafassouam@gmail.com>
2023-11-20 10:47:44 -0800

    fixing a requirement by adding an if h is not null conditon

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,14 +14,14 @@ size_t print_listint(const listint_t *h)
 	size_t i;
 
 	i = 0;
-
-	while (h)
+	if (h != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		i++;
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			i++;
+		}
 	}
-
-	return (i);
+		return (i);
 }
-
