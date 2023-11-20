cc5fada5d828efba465afdcc23b1355531306637
achraf assouam <achrafassouam@gmail.com>
2023-11-20 12:50:56 -0800

    updating 0-print_listint.c file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,12 +4,17 @@
  * print_listint - prints all the elements of a listint_t list
  * @h: point to elements of the list
  *
- * Return: node count
+ * Return: node count, or 0 if h is null
  */
 
 size_t print_listint(const listint_t *h)
 {
 	size_t node_count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
 
 	while (h != NULL)
 	{
@@ -17,5 +22,6 @@ size_t print_listint(const listint_t *h)
 		h = h->next;
 		node_count++;
 	}
+
 	return (node_count);
 }
