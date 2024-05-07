ab340119ff177386e8e6144eeb453d0e4d3f8846
“Achraf <achrafassouam@gmail.com>
2024-05-07 14:45:30 -0700

    head file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGO_H
+#define SEARCH_ALGO_H
+
+/* HEADERS */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* PROTOTYPES  */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif
